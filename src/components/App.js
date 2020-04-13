023697ef6bec0e7660df6eeb739fce9be1ce49ac
Alan Curtis <alancurtis5000@gmail.com>
2020-04-13 15:48:02 -0600

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from './Button.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button />
+        <h1>React App!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
